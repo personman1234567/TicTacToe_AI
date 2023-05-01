9b2929f32254c136ca4974517303ae41e0e5065c
Ryan Christopher <ryandchristopher27@gmail.com>
2023-05-01 15:12:16 -0500

    Added logistic regression and formatted UI

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from TicTacToe import *
 from TicTacToe_MiniMax import *
 from minRiskClassifier import *
 import tkinter.font as tkFont
+from logisticRegressionClassifier import *
 
 class TicTacToeGUI:
     def __init__(self):
@@ -18,48 +19,80 @@ class TicTacToeGUI:
 
         self.buttons = [[" " for _ in range(3)] for _ in range(3)]
 
+        # Create tic tac toe board
         for i in range(3):
             for j in range(3):
                 button = tk.Button(self.root, text="", width=10, height=5, command=lambda row=i, col=j: self.clicked(row, col))
                 button.grid(row=i, column=j)
                 self.buttons[i][j] = button
 
-        self.nextTurnVal = tk.Label(self.root, text="X")
-        self.nextTurnVal.grid(row=0, column=4)
-        self.nextTurnLabel = tk.Label(self.root, text="Next Turn:")
-        self.nextTurnLabel.grid(row=0, column=3)
-
-        nextMove = getNextBestMoveMinRisk([0,0,0,0,0,0,0,0,0], 1)
-        move = nextMove[0]
-
-        # Label and Value for Minimum Risk Classifier for X
-        self.xRecVal = tk.Label(self.root, text=str(move))
-        self.xRecVal.grid(row=1, column=4)
-        self.xRecLabel = tk.Label(self.root, text="MinRisk Classifier for X:")
-        self.xRecLabel.grid(row=1, column=3)
-
-        # Label and Value for Game Status
-        self.statusLabel = tk.Label(self.root, text="Round")
-        self.statusLabel.grid(row=3, column=3)
-        self.statusVal = tk.Label(self.root, text=self.round)
-        self.statusVal.grid(row=3, column=4)
+        # Create Reset Button
+        resetButton = tk.Button(self.root, text="Reset", width=10, height=5, command=lambda: self.resetGame())
+        resetButton.grid(row=3, column=0)
 
         # Label and Value for board map
         self.mapLabel = tk.Label(self.root, text="Map: ")
-        self.mapLabel.grid(row=3, column=0)
+        self.mapLabel.grid(row=3, column=1)
         self.mapVal = tk.Label(self.root, text="1|2|3\n4|5|6\n7|8|9")
         f = tkFont.Font(self.mapVal, self.mapVal.cget("font"))
         f.configure(underline = True)
         self.mapVal.configure(font=f)
-        self.mapVal.grid(row=3, column=1)
+        self.mapVal.grid(row=3, column=2)
+        
+        # Create Current Round Val and Label
+        self.currentRoundLabel = tk.Label(self.root, text="Round:")
+        self.currentRoundLabel.grid(row=0, column=3)
+        self.currentRoundVal = tk.Label(self.root, text=self.round)
+        self.currentRoundVal.grid(row=0, column=4)
+
+        # Create Current Turn Val and Label
+        self.currentTurnLabel = tk.Label(self.root, text="Current Turn:")
+        self.currentTurnLabel.grid(row=1, column=3)
+        self.currentTurnVal = tk.Label(self.root, text="X")
+        self.currentTurnVal.grid(row=1, column=4)
+        
+        # Label and Value for Game Status
+        self.statusLabel = tk.Label(self.root, text="Status:")
+        self.statusLabel.grid(row=2, column=3)
+        self.statusVal = tk.Label(self.root, text="Ongoing")
+        self.statusVal.grid(row=2, column=4)
+
+
+        # Label and Value for Classifiers Column
+        self.classifierHeaderLabel = tk.Label(self.root, text="Classsifiers for:", width=25, height=5, borderwidth=1, relief="ridge")
+        self.classifierHeaderLabel.grid(row=0, column=5)
+        self.classifierHeaderVal = tk.Label(self.root, text="X", width=10, height=5, borderwidth=1, relief="ridge")
+        self.classifierHeaderVal.grid(row=0, column=6)
+        self.classifierHeaderProb = tk.Label(self.root, text="Probability of Positive", width=20, height=5, borderwidth=1, relief="ridge")
+        self.classifierHeaderProb.grid(row=0, column=7)
+
+
+        minRiskMoveAndProb = getNextBestMoveMinRisk([0,0,0,0,0,0,0,0,0], 1)
+        # Label and Value for Minimum Risk Classifier for X
+        self.minRiskXClassifierLabel = tk.Label(self.root, text="MinRisk Classifier:", width=25, height=5, borderwidth=1, relief="ridge")
+        self.minRiskXClassifierLabel.grid(row=1, column=5)
+        self.minRiskXClassifierVal = tk.Label(self.root, text=str(minRiskMoveAndProb[0]))
+        self.minRiskXClassifierVal.grid(row=1, column=6)
+        self.minRiskXClassifierProb = tk.Label(self.root, text=str(round(minRiskMoveAndProb[1], 5)))
+        self.minRiskXClassifierProb.grid(row=1, column=7)
+
+
+        logisticRegressionMoveAndProb = logisticRegressionClassifier([0,0,0,0,0,0,0,0,0], 1)
+        # Label and Value for Logistic Regression Classifier for X
+        self.logRegXClassifierLabel = tk.Label(self.root, text="Logistic Regression Classifier:", width=25, height=5, borderwidth=1, relief="ridge")
+        self.logRegXClassifierLabel.grid(row=2, column=5)
+        self.logRegXClassifierVal = tk.Label(self.root, text=str(logisticRegressionMoveAndProb[0]))
+        self.logRegXClassifierVal.grid(row=2, column=6)
+        self.logRegXClassifierProb = tk.Label(self.root, text=str(round(logisticRegressionMoveAndProb[1], 5)))
+        self.logRegXClassifierProb.grid(row=2, column=7)
+
 
         nextMinimaxMove = get_best_move_minimax(self.board)
-
         # Label and Value for Minimax Recommendation
-        self.minimaxLabel = tk.Label(self.root, text="Minimax Recommendation for X:")
-        self.minimaxLabel.grid(row=2, column=3)
+        self.minimaxLabel = tk.Label(self.root, text="Minimax Classifier:", width=25, height=5, borderwidth=1, relief="ridge")
+        self.minimaxLabel.grid(row=3, column=5)
         self.minimaxVal = tk.Label(self.root, text=str(self.colRowToVectorVal(nextMinimaxMove[1], nextMinimaxMove[0])))
-        self.minimaxVal.grid(row=2, column=4)
+        self.minimaxVal.grid(row=3, column=6)
 
 
     def setPlayer(self, player):
@@ -101,11 +134,45 @@ class TicTacToeGUI:
             self.setStatusLabel("Draw!")
             self.setStatusVal("")
 
-    def setTurnLabel(self, content):
-        self.nextTurnVal["text"] = content
+    def resetGame(self):
+        self.setRound(1)
+        self.setRoundVal("1")
+        self.setPlayer("X")
+        self.setTurnLabel(self.player)
+        self.setStatusLabel("Status:")
+        self.setStatusVal("Ongoing")
 
-    def setXRecLabel(self, content):
-        self.xRecVal["text"] = content
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i][j]["text"] = ""
+                self.board[i][j] = ' '
+
+        minRiskMoveAndProb = getNextBestMoveMinRisk([0,0,0,0,0,0,0,0,0], 1)
+        self.setMinRiskXVal(minRiskMoveAndProb[0])
+        self.setMinRiskXProb(round(minRiskMoveAndProb[1], 5))
+
+        logisticRegressionMoveAndProb = logisticRegressionClassifier([0,0,0,0,0,0,0,0,0], 1)
+        self.setLogRegXVal(logisticRegressionMoveAndProb[0])
+        self.setLogRegXProb(round(logisticRegressionMoveAndProb[1], 5))
+
+        nextMinimaxMove = get_best_move_minimax(self.board)
+        self.setMinimaxVal(str(self.colRowToVectorVal(nextMinimaxMove[1], nextMinimaxMove[0])))
+
+
+    def setTurnLabel(self, content):
+        self.currentTurnVal["text"] = content
+
+    def setMinRiskXVal(self, content):
+        self.minRiskXClassifierVal["text"] = content
+
+    def setMinRiskXProb(self, content):
+        self.minRiskXClassifierProb["text"] = content
+
+    def setLogRegXVal(self, content):
+        self.logRegXClassifierVal["text"] = content
+
+    def setLogRegXProb(self, content):
+        self.logRegXClassifierProb["text"] = content
 
     def setStatusLabel(self, content):
         self.statusLabel["text"] = content
@@ -116,12 +183,15 @@ class TicTacToeGUI:
     def setRound(self, round):
         self.round = round
 
+    def setRoundVal(self, content):
+        self.currentRoundVal["text"] = content
+
     def setMinimaxVal(self, content):
         self.minimaxVal["text"] = content
 
     def updateRoundDisplay(self):
         self.setRound(self.round + 1)
-        self.setStatusVal(self.round)
+        self.setRoundVal(self.round)
 
     def updateWinnerDisplay(self, winner):
         self.setStatusLabel("Winner: ")
@@ -174,14 +244,17 @@ class TicTacToeGUI:
             player = 0
 
         if self.player == "X":
-            nextMove = getNextBestMoveMinRisk(board, player)
+            nextMoveMinRisk = getNextBestMoveMinRisk(board, player)
+            nextMoveLogReg = logisticRegressionClassifier(board, player)
             nextMoveMinimax = get_best_move_minimax(self.board)
 
-            move = nextMove[0]
-            self.setXRecLabel(str(move))
+            self.setMinRiskXVal(str(nextMoveMinRisk[0]))
+            self.setMinRiskXProb(str(round(nextMoveMinRisk[1], 5)))
 
-            moveMinimax = self.colRowToVectorVal(nextMoveMinimax[1], nextMoveMinimax[0])
-            self.setMinimaxVal(str(moveMinimax))
+            self.setLogRegXVal(str(nextMoveLogReg[0]))
+            self.setLogRegXProb(str(round(nextMoveLogReg[1], 5)))
+
+            self.setMinimaxVal(str(self.colRowToVectorVal(nextMoveMinimax[1], nextMoveMinimax[0])))
     
     def colRowToVectorVal(self, col, row):
         vectorVal = (row*3) + (col+1)
---
+++ logisticRegressionClassifier.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from minRiskClassifier import *
+import pandas as pd
+
+def logisticRegressionClassifier(state, player):
+    
+    data = pd.read_csv("dataNums.csv")
+
+    stateLabels = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    x = data[stateLabels]
+    y = data["class"]
+    stateCombined = dict()
+    for index, val in enumerate(stateLabels):
+        stateCombined[val] = state[index]
+    stateDF = pd.DataFrame(stateCombined, index=[0])
+
+    # Create Logistic Regression Model
+    model = LogisticRegression(random_state=0).fit(x, y)
+
+    # Get Next board states
+    nextStates = getNextBoardStates(state, player)
+
+    highestProbability = (0,0)
+    for key, value in nextStates.items():
+        # Format new board state
+        stateCombined = dict()
+        for index, val in enumerate(stateLabels):
+            stateCombined[val] = value[index]
+        stateDF = pd.DataFrame(stateCombined, index=[0])
+
+        # Find probability of each new board state
+        probabilities = model.predict_proba(stateDF)
+
+        if probabilities[0][0] > highestProbability[1]:
+            highestProbability = (key, probabilities[0][0])
+        # print(probabilities[0][0])
+        # print(probabilities[0][1])
+
+    return highestProbability
+
+
+
+
+# prob = logisticRegressionClassifier([1,0,0,0,-1,0,0,0,0], 1)
+# print(prob)
