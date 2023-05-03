f38b006438333c167f5d4c22cff57f89c84cc1c5
Ryan Christopher <ryandchristopher27@gmail.com>
2023-05-03 15:16:38 -0500

    adding new minrisk model

--- minRiskClassifier.py
+++ minRiskClassifier.py
@@ -1,10 +1,13 @@
 # Helpful Links
     # https://machinelearningmastery.com/naive-bayes-classifier-scratch-python/
+    # https://scikit-learn.org/stable/modules/naive_bayes.html#gaussian-naive-bayes
 
 import csv
 import copy
 from math import exp, pi, sqrt
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 def calculateProbability(x, mean, stdev):
     exponenet = exp(-((x-mean)**2 / (2 * stdev**2 )))
@@ -86,6 +89,47 @@ def getNextBestMoveMinRisk(state, player):
                 highestProbability = (key, probabilities['negative'])
 
     return highestProbability
+
+def minRiskClassifier(state, player):
+
+    data = pd.read_csv("dataNums.csv")  
+
+    X = data.drop("class", axis=1)
+    y = data["class"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+
+    gnb = GaussianNB().fit(X_train, y_train)
+
+    # Evalute the model
+    # y_pred = gnb.predict(X_test)
+    # accuracy = accuracy_score(y_test, y_pred)
+    # print("Accuracy:", accuracy)
+
+    nextStates = getNextBoardStates(state, player)
+
+    stateLabels = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    highestProbability = (0,0)
+    for key, value in nextStates.items():
+        # Format new board state
+        stateCombined = dict()
+        for index, val in enumerate(stateLabels):
+            stateCombined[val] = value[index]
+        stateDF = pd.DataFrame(stateCombined, index=[0])
+
+        # Find probability of each new board state
+        probabilities = gnb.predict_proba(stateDF)
+
+        if player == 1:
+            if probabilities[0][0] > highestProbability[1]:
+                highestProbability = (key, probabilities[0][0])
+        else:
+            if probabilities[0][1] > highestProbability[1]:
+                highestProbability = (key, probabilities[0][1])
+
+    return highestProbability
+
 
 
 # ************ Testing ******************
